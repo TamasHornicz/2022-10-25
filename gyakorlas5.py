bef0ba3680b2471121d4c16753905439c32ef91f
TamasHornicz <tamashornicz@gmail.com>
2022-10-28 10:35:38 +0200

    ellenőrzés

--- gyakorlas5.py
+++ gyakorlas5.py
@@ -1,9 +1,30 @@
 import random
 
-
+'''
 hossz = int(input("Adja meg a hosszát: "))
 
 for i in range(hossz):
     lista = [*range(-100, 100+1)]
     veletlen = random.choice(lista)
-    print(veletlen)
+    print(veletlen)'''
+
+N = int(input("Írja be hogy mennyi számot szeretne: "))
+
+Lszamok= []
+
+for i in range(N): 
+    Lszamok.append(random.randint(-100, 100))
+
+negativdb = 0
+
+for szam in Lszamok:
+    if(szam<0):
+        negativdb+=1
+
+print("Negatí számok száma:", negativdb)
+
+print(max(Lszamok)-min(Lszamok))
+
+Lszamok.sort()
+Lszamok.reverse
+print(Lszamok)
